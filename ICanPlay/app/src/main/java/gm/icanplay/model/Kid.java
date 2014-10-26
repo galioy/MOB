10e79318a91a75438c90957c6e35bdaf9efee28c
galioy <galioy1024@gmail.com>
2014-10-27 00:02:07 +0100

    Create second activity for 'Can play kids' list. Refactor the IDs of the phone and school text fields. Create phone and groupid fields in 'Kid' class and create constructor for the class.

--- ICanPlay/app/src/main/java/gm/icanplay/model/Kid.java
+++ ICanPlay/app/src/main/java/gm/icanplay/model/Kid.java
@@ -7,6 +7,31 @@ public class Kid {
     private String name;
     private boolean canPlay = false;
     private String phone;
+    private String groupid;
+
+    public Kid(String _name, boolean _canPlay, String _phone, String _groupid)
+    {
+        this.name = _name;
+        this.canPlay = _canPlay;
+        this.phone = _phone;
+        this.groupid = _groupid;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(String groupid) {
+        this.groupid = groupid;
+    }
 
     public String getName() {
         return name;
