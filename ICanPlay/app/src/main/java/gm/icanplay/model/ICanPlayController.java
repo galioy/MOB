68f855ab46fb30d850bd19297989a49a4c0a1e52
galioy <galioy1024@gmail.com>
2014-11-03 11:29:53 +0100

    Refactor: remove unused code.

--- ICanPlay/app/src/main/java/gm/icanplay/model/ICanPlayController.java
+++ ICanPlay/app/src/main/java/gm/icanplay/model/ICanPlayController.java
@@ -10,12 +10,4 @@ public class ICanPlayController {
     public void buttonAction(String name, String group, String phone, String feed, StartScreen activity, String cmd) {
         new ButtonsAsyncTask(activity).execute(name, group, phone, feed, cmd);
     }
-
-//    public void kidCanPlay(String name, String group, String feed, StartScreen activity) {
-//        new CanPlayTask(activity).execute(name, group, feed);
-//    }
-//
-//    public void kidCannotPlay(String name, String group, String feed, StartScreen activity) {
-//        new CannotPlayTask(activity).execute(name, group, feed);
-//    }
 }
