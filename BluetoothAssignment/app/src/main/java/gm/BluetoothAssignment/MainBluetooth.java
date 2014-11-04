6c3f2b7a006da5628812dea33adca93c32422dbd
wateva <mkyukov@gmail.com>
2014-11-04 11:13:07 +0100

    Working bluetooth conn...

--- BluetoothAssignment/app/src/main/java/gm/BluetoothAssignment/MainBluetooth.java
+++ BluetoothAssignment/app/src/main/java/gm/BluetoothAssignment/MainBluetooth.java
@@ -19,9 +19,9 @@ import android.widget.TextView;
 
 
 public class MainBluetooth extends Activity {
-	private BluetoothAdapter btAdapter;
-	private BroadcastReceiver mReceiver;
-	private BluetoothDevice mDevice;
+    private BluetoothAdapter btAdapter;
+    private BroadcastReceiver mReceiver;
+    private BluetoothDevice mDevice;
     private static TextView output_view;
 
     @Override
@@ -30,96 +30,98 @@ public class MainBluetooth extends Activity {
     }
 
     @Override
-	protected void onPause() {
-		super.onPause();
-		if (btAdapter.isDiscovering()) {
-			btAdapter.cancelDiscovery();
-		}
-		this.unregisterReceiver(mReceiver);
-	}
+    protected void onPause() {
+        super.onPause();
+        if (btAdapter.isDiscovering()) {
+            btAdapter.cancelDiscovery();
+        }
+        this.unregisterReceiver(mReceiver);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_bluetooth);
-		Button button = (Button) findViewById(R.id.button1);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bluetooth);
+        Button button = (Button) findViewById(R.id.button1);
         output_view = (TextView) findViewById(R.id.TV_output);
 
+        OnClickListener mCorkyListener = new OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("enableBluetooth");
+                output_view.append("enableBluetooth\n");
+                enableBT();
+            }
+        };
+        button.setOnClickListener(mCorkyListener);
+    }
 
-		OnClickListener mCorkyListener = new OnClickListener() {
-			public void onClick(View v) {
-//				System.out.println("enableBluetooth");
-                output_view.append("enableBluetooth");
-				enableBT();
-			}
-		};
-		button.setOnClickListener(mCorkyListener);
-	}
-
-	private void enableBT() {
-		btAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (btAdapter.isEnabled() == false) {
-			btAdapter.enable();
-		}
+    private void enableBT() {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter.isEnabled() == false) {
+            btAdapter.enable();
+        }
         Set<BluetoothDevice> myDevice = btAdapter.getBondedDevices();
-		mDevice = btAdapter.getRemoteDevice("BC:77:37:6F:A0:3F");
+        mDevice = btAdapter.getRemoteDevice("BC:77:37:6F:A0:3F");
 
 //		System.out.println("Try to get a socket");
         output_view.append("Try to get a socket...\n");
 
-		BluetoothSocket socket;
+        BluetoothSocket socket;
 
-		try {                                                                  
-			socket = mDevice.createRfcommSocketToServiceRecord(UUID.fromString("27012F0C-68AF-4FBF-8DBE-6BBAF7AA432A"));
-//			System.out.println("Got a socket");
-            output_view.append("Got a socket...\n");
-			Thread thr = new ConnectThread(socket);
-			thr.start();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+        try {
+            socket = mDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+            System.out.println("Got a socket");
+//            output_view.append("Got a socket...\n");
+            Thread thr = new ConnectThread(socket);
+            btAdapter.cancelDiscovery();
+            thr.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_bluetooth, menu);
-		return true;
-	}
-	
-	private static class ConnectThread extends Thread
-	{
-		private BluetoothSocket socket;
-		
-		public ConnectThread(BluetoothSocket socket) {
-			this.socket = socket;
-		}
+        }
+    }
 
-		@Override
-		public void run() {
-//			System.out.println("Connecting..");
-            output_view.append("Connecting...\n");
-			try {
-				socket.connect();
-//				System.out.println("connection established");
-                output_view.append("Connecting...\n");
-				try {
-					OutputStream output = socket.getOutputStream();
-					output.write('X');
-					output.flush();
-					Thread.sleep(5000);
-					socket.close();
-				} catch (IOException e) {
-					System.out.println(e.getMessage());
-				} catch (InterruptedException e) {
-					System.out.println(e.getMessage());
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_bluetooth, menu);
+        return true;
+    }
 
-				}
+    private static class ConnectThread extends Thread {
+        private BluetoothSocket socket;
 
-				System.out.println("DESTROYED");
-			} catch (IOException e1) {
-				System.out.println(e1.getMessage());
-			}
-		}		
-	}
+        public ConnectThread(BluetoothSocket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Connecting..");
+//            output_view.append("Connecting...\n");
+            try {
+
+                socket.connect();
+                System.out.println("connection established");
+//                output_view.append("Connection established!!!\n");
+                try {
+                    OutputStream output = socket.getOutputStream();
+                    output.write('X');
+                    output.flush();
+                    Thread.sleep(5000);
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                System.out.println("DESTROYED");
+
+            } catch (IOException e1) {
+//                output_view.append("IOException!");
+                System.out.println(e1.getMessage());
+            }
+        }
+    }
 }
